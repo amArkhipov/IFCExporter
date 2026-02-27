013865abb4846501db09213b4b2f3b4eba61d292
Andrey Arkhipov <arhidrey@gmail.com>
2026-02-27 15:43:12 +0300

    Added base  IExternalCommand

---
+++ IFCExporter.RevitCommand/IFCExporterCommand.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace IFCExporter.RevitCommand
+{
+    public class IFCExporterCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex) 
+            {
+                message = ex.Message;
+                message += ex.StackTrace;
+                return Result.Failed;
+            }
+        }
+    }
+}
