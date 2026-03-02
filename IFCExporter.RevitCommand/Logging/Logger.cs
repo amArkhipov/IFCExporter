fc21adb744c8e348ce79d50669b96c12a6a5bd39
Andrey Arkhipov <arhidrey@gmail.com>
2026-03-02 08:45:34 +0300

    Added test logger

---
+++ IFCExporter.RevitCommand/Logging/Logger.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFCExporter.RevitCommand
+{
+    public class Logger : iLogger
+    {
+        public void LogInfo(string message)
+        {
+            TaskDialog mainDialog = new TaskDialog("Info");
+            mainDialog.MainIcon = TaskDialogIcon.TaskDialogIconInformation;
+            mainDialog.MainInstruction = message;
+            mainDialog.Show();
+        }
+        public void LogError(Exception ex)
+        {
+            TaskDialog mainDialog = new TaskDialog(ex.Source);
+            mainDialog.MainIcon = TaskDialogIcon.TaskDialogIconError;
+            mainDialog.MainInstruction = ex.Source;
+            mainDialog.MainContent = ex.Message;
+            mainDialog.ExpandedContent = ex.StackTrace;
+            mainDialog.Show();
+        }
+    }
+}
